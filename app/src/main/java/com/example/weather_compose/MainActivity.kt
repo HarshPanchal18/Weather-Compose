bf1d8366a02d9163c2359ad04cd989ade1cf5f29
Harsh Panchal <harshhhh1803@gmail.com>
2023-07-10 12:22:20 +0530

    Implemented Blue-White gradient background, Aligned CircularProgressIndicator to the Center by wrapping it with Box, Made button background blue and applied elevation, Formatted Text Output with Emojis and more styles, Created a function which returns wind direction in full form instead of code, and minor fixes

--- app/src/main/java/com/example/weather_compose/MainActivity.kt
+++ app/src/main/java/com/example/weather_compose/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -27,6 +27,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -44,6 +45,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.TextUnit
@@ -53,6 +55,7 @@ import coil.compose.rememberAsyncImagePainter
 import com.example.weather_compose.model.CurrentWeatherResponse
 import com.example.weather_compose.ui.theme.WeatherComposeTheme
 import com.example.weather_compose.viewModel.MainViewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
 
@@ -62,6 +65,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            rememberSystemUiController().setStatusBarColor(color = Color.White)
             WeatherComposeTheme {
                 val cityInput = remember { mutableStateOf("") }
                 val trailingIconView = @Composable {
@@ -73,12 +77,22 @@ class MainActivity : ComponentActivity() {
 
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize().background(brush = Brush.verticalGradient(listOf(Color.Blue.copy(0.25F),Color.White))),
-                    color = Color.Blue.copy(0.2f)
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.onSurface
                 ) {
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
+                            .background(
+                                brush = Brush.verticalGradient(
+                                    listOf(
+                                        Color.Blue.copy(0.25F),
+                                        Color(0xFF00A5FD),
+                                        Color(0xFF4EB8F1),
+                                        Color.White
+                                    )
+                                )
+                            )
                             .padding(10.dp)
                         //horizontalAlignment = Alignment.CenterHorizontally
                     ) {
@@ -94,10 +108,6 @@ class MainActivity : ComponentActivity() {
                                     .background(color = Color.White, shape = roundShape)
                                     .shadow(elevation = 2.dp, shape = roundShape)
                                     .clip(shape = roundShape),
-                                /*.border(
-                                    border = BorderStroke(1.dp, Color.Blue.copy(0.5F)),
-                                    shape = RoundedCornerShape(12.dp)
-                                ),*/
                                 colors = TextFieldDefaults.textFieldColors(
                                     containerColor = Color.LightGray.copy(0.20f),
                                     cursorColor = Color.Blue.copy(0.35F),
@@ -128,29 +138,14 @@ class MainActivity : ComponentActivity() {
                                 ),
                             )
                             Spacer(Modifier.width(10.dp))
-                            Button(
-                                onClick = {
-                                    if (cityInput.value.isNotBlank()) {
-                                        mainViewModel.getWeatherData(cityInput.value.trim())
-                                    } else {
-                                        Toast.makeText(
-                                            this@MainActivity,
-                                            "Enter the city please!!",
-                                            Toast.LENGTH_SHORT
-                                        ).show()
-                                    }
-                                },
-                                colors = ButtonDefaults.buttonColors(
-                                    containerColor = Color.Blue.copy(0.35F)
-                                ),
-                                elevation = ButtonDefaults.buttonElevation(10.dp)
-                            ) {
-                                Text(text = "Get Weather")
-                            }
+                            GetWeatherButton(city = cityInput.value)
                         } // Row
 
                         mainViewModel.isLoading.observeAsState().value.let { isLoading ->
-                            if (isLoading == true) CircularProgressIndicator()
+                            if (isLoading == true)
+                                Box(modifier = Modifier.fillMaxSize()) {
+                                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                                }
                         }
 
                         mainViewModel.isError.observeAsState().value.let { isError ->
@@ -161,9 +156,32 @@ class MainActivity : ComponentActivity() {
                             SetResultText(weatherData = data)
                         }
                     }
-                }
+                } // Surface
             }
         } // setContent
+    }
+
+    @Composable
+    fun GetWeatherButton(city: String) {
+        Button(
+            onClick = {
+                if (city.isNotBlank()) {
+                    mainViewModel.getWeatherData(city.trim())
+                } else {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Enter the city please!!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue.copy(0.35F),
+                disabledContainerColor = Color.LightGray
+            ),
+            enabled = city.isNotEmpty(),
+            elevation = ButtonDefaults.buttonElevation(10.dp)
+        ) { Text(text = "Get Weather") }
     }
 
     @Composable
@@ -173,31 +191,72 @@ class MainActivity : ComponentActivity() {
             horizontalAlignment = Alignment.Start
         ) {
             if (weatherData != null) {
-                weatherData.current.let { current ->
-                    Row(modifier = Modifier.height(200.dp)) {
-                        Text(
-                            text = "${current?.tempC}\u2103",
-                            fontSize = 50.sp,
-                            modifier = Modifier.weight(1F)
-                        )
-                        current?.condition.let { condition ->
-                            Column(modifier = Modifier.weight(1F)) {
-                                SetResultImage(condition?.icon)
-                                Text("${condition?.text}")
-                            }
-                        }
-                    }
-                    /*Text(
-                        text = "${current?.tempF}\u2109",
-                        fontSize = 20.sp
-                    )*/
+                weatherData.location.let { location ->
+                    Text(
+                        text = "📍 ${location?.name}, ${location?.region}, ${location?.country}",
+                        color = Color.White,
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily.SansSerif,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "\uD83C\uDF10 ${location?.tzId}",
+                        color = Color.White,
+                        fontSize = 18.sp,
+                        fontFamily = FontFamily.Serif,
+                    )
+                    Text(
+                        text = "⌚ ${location?.localtime}",
+                        color = Color.White,
+                        fontSize = 18.sp,
+                        fontFamily = FontFamily.Serif,
+                    )
                 }
 
-                weatherData.location.let { location ->
-                    Text("${location?.name}, ${location?.region}, ${location?.country}")
-                    Text("Timezone: ${location?.tzId}")
-                    Text("Local Time: ${location?.localtime}")
-                    Text("${location?.lat}:${location?.lon}")
+                weatherData.current.let { current ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 15.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "${current?.tempC} \u2103",
+                            fontSize = 70.sp,
+                            color = Color.White,
+                            fontFamily = FontFamily.Serif,
+                            fontWeight = FontWeight.ExtraBold
+                        )
+                        current?.condition.let { condition ->
+                            SetResultImage(condition?.icon)
+                            Text(
+                                text = "${condition?.text}\n",
+                                color = Color.White,
+                                fontSize = 25.sp,
+                                fontFamily = FontFamily.Serif,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    } // Column
+
+                    Text(
+                        text = "🌀 ${getWindDirection(current?.windDir)}, ${current?.windMph ?: "N/A"} mph (${current?.windKph ?: "N/A"} kmph)",
+                        color = Color.White,
+                        fontSize = 18.sp,
+                        fontFamily = FontFamily.Serif,
+                    )
+                    Text(
+                        text = "💧 ${current?.humidity}%",
+                        color = Color.White,
+                        fontSize = 18.sp,
+                        fontFamily = FontFamily.Serif,
+                    )
+                    Text(
+                        text = "🔦 ${current?.visMiles ?: "N/A"} miles (${current?.visKm ?: "N/A"} kms)",
+                        color = Color.White,
+                        fontSize = 18.sp,
+                        fontFamily = FontFamily.Serif,
+                    )
                 }
             } // if
         } // Column
@@ -212,7 +271,7 @@ class MainActivity : ComponentActivity() {
                     painter = painter, contentDescription = "Weather Photo",
                     modifier = Modifier
                         .align(Alignment.Center)
-                        .size(160.dp)
+                        .size(180.dp)
                         .clip(RoundedCornerShape(10)),
                     contentScale = ContentScale.FillBounds
                 )
@@ -220,75 +279,25 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    /*@OptIn(ExperimentalMaterial3Api::class)
-    @Preview(showBackground = true)
-    @Composable
-    fun Previews() {
-        WeatherComposeTheme {
-
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Row(
-                    modifier = Modifier.padding(bottom = 20.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    TextField(
-                        value = "",
-                        onValueChange = { },
-                        modifier = Modifier
-                            .weight(1F)
-                            .background(color = Color.White, shape = RoundedCornerShape(10.dp))
-                            .shadow(elevation = 2.dp, shape = RoundedCornerShape(10.dp))
-                            .clip(shape = RoundedCornerShape(10.dp)),
-                        /*.border(
-                            border = BorderStroke(1.dp, Color.Blue.copy(0.5F)),
-                            shape = RoundedCornerShape(12.dp)
-                        ),*/
-                        colors = TextFieldDefaults.textFieldColors(
-                            containerColor = Color.LightGray.copy(0.20f),
-                            cursorColor = Color.Blue.copy(0.35F),
-                            focusedIndicatorColor = Color.Transparent,
-                            unfocusedIndicatorColor = Color.Transparent,
-                            disabledIndicatorColor = Color.Transparent,
-                            errorIndicatorColor = Color.Transparent,
-                        ),
-                        placeholder = { Text("Enter City") },
-                        singleLine = true,
-                        leadingIcon = {
-                            Icon(
-                                imageVector = Icons.Default.Place,
-                                contentDescription = null,
-                                tint = Color.Green.copy(0.6F)
-                            )
-                        },
-                        //trailingIcon = if (cityInput.value.isNotEmpty()) trailingIconView else null,
-                        keyboardOptions = KeyboardOptions(
-                            autoCorrect = true,
-                            keyboardType = KeyboardType.Text,
-                            imeAction = ImeAction.Search
-                        ),
-                        textStyle = TextStyle(
-                            color = Color.Black,
-                            fontSize = TextUnit.Unspecified,
-                            fontFamily = FontFamily.SansSerif
-                        ),
-                    )
-                    Spacer(Modifier.width(10.dp))
-                    Button(
-                        onClick = {},
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = Color.Blue.copy(0.35F)
-                        ),
-                        elevation = ButtonDefaults.buttonElevation(10.dp)
-                    ) {
-                        Text(text = "Get Weather")
-                    }
-                } // Row
-            }
+    private fun getWindDirection(direction: String?): String {
+        return when (direction) {
+            "N" -> "North" // 0, 360
+            "NNE" -> "North-Northeast" // 22.5
+            "NE" -> "Northeast" // 45
+            "ENE" -> "East-Northeast" // 67.5
+            "E" -> "East" // 90
+            "ESE" -> "East-Southeast" // 112.5
+            "SE" -> "Southeast" // 135
+            "SSE" -> "South-Southeast" // 157.5
+            "S" -> "South" // 180
+            "SSW" -> "South-Southwest" // 202.5
+            "SW" -> "Southwest" // 225
+            "WSW" -> "West-Southwest" // 247.5
+            "W" -> "West" // 270
+            "WNW" -> "West-Northwest" // 292.5
+            "NW" -> "Northwest" // 315
+            "NNW" -> "North-Northwest" // 337.5
+            else -> "N/A"
         }
-    } // Row*/
+    }
 }
